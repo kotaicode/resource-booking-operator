55a5dc03d7ba44c6f5e6799ef384c83777feb6f9
Dimitar Ralev <dralev@shiftingphotons.dev>
2022-12-01 17:09:37 +0200

    Log initial progress on adding tests

---
+++ controllers/resource_controller_test.go
@@ -0,0 +1,64 @@
+package controllers
+
+import (
+	"context"
+	"time"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+
+	managerv1 "github.com/kotaicode/resource-booking-operator/api/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+	//+kubebuilder:scaffold:imports
+)
+
+var _ = Describe("Resource controller", func() {
+
+	// Define utility constants for object names and testing timeouts/durations and intervals.
+	const (
+		ResourceName      = "test-resource"
+		ResourceTag       = "analytics"
+		ResourceNamespace = "default"
+
+		timeout  = time.Second * 10
+		duration = time.Second * 10
+		interval = time.Millisecond * 250
+	)
+
+	Context("When updating CronJob Status", func() { // TODO
+		It("Should increase CronJob Status.Active count when new Jobs are created", func() { // TODO
+			By("By creating a new Resource")
+			ctx := context.Background()
+			resource := &managerv1.Resource{
+				TypeMeta: metav1.TypeMeta{
+					APIVersion: "manager.kotaico.de/v1",
+					Kind:       "Resource",
+				},
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      ResourceName,
+					Namespace: ResourceNamespace,
+				},
+				Spec: managerv1.ResourceSpec{
+					Booked: false,
+					Tag:    ResourceTag,
+				},
+			}
+			Expect(k8sClient.Create(ctx, resource)).Should(Succeed())
+
+			// Check that the spec we passed are matching
+			resourceLookupKey := types.NamespacedName{Name: ResourceName, Namespace: ResourceNamespace}
+			createdResource := &managerv1.Resource{}
+
+			Eventually(func() bool {
+				err := k8sClient.Get(ctx, resourceLookupKey, createdResource)
+				if err != nil {
+					return false
+				}
+				return true
+			}, timeout, interval).Should(BeTrue())
+
+			Expect(createdResource.Spec.Tag).Should(Equal("chep mostly"))
+		})
+	})
+})
--- controllers/suite_test.go
+++ controllers/suite_test.go
@@ -17,6 +17,7 @@ limitations under the License.
 package controllers
 
 import (
+	"context"
 	"path/filepath"
 	"testing"
 
@@ -25,6 +26,7 @@ import (
 
 	"k8s.io/client-go/kubernetes/scheme"
 	"k8s.io/client-go/rest"
+	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/envtest"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
@@ -37,9 +39,13 @@ import (
 // These tests use Ginkgo (BDD-style Go testing framework). Refer to
 // http://onsi.github.io/ginkgo/ to learn more about Ginkgo.
 
-var cfg *rest.Config
-var k8sClient client.Client
-var testEnv *envtest.Environment
+var (
+	cfg       *rest.Config
+	k8sClient client.Client
+	testEnv   *envtest.Environment
+	ctx       context.Context
+	cancel    context.CancelFunc
+)
 
 func TestAPIs(t *testing.T) {
 	RegisterFailHandler(Fail)
@@ -49,6 +55,7 @@ func TestAPIs(t *testing.T) {
 
 var _ = BeforeSuite(func() {
 	logf.SetLogger(zap.New(zap.WriteTo(GinkgoWriter), zap.UseDevMode(true)))
+	ctx, cancel = context.WithCancel(context.TODO())
 
 	By("bootstrapping test environment")
 	testEnv = &envtest.Environment{
@@ -71,9 +78,33 @@ var _ = BeforeSuite(func() {
 	Expect(err).NotTo(HaveOccurred())
 	Expect(k8sClient).NotTo(BeNil())
 
+	k8sManager, err := ctrl.NewManager(cfg, ctrl.Options{
+		Scheme: scheme.Scheme,
+	})
+	Expect(err).ToNot(HaveOccurred())
+
+	err = (&ResourceReconciler{
+		Client: k8sManager.GetClient(),
+		Scheme: k8sManager.GetScheme(),
+	}).SetupWithManager(k8sManager)
+	Expect(err).ToNot(HaveOccurred())
+
+	err = (&BookingReconciler{
+		Client: k8sManager.GetClient(),
+		Scheme: k8sManager.GetScheme(),
+	}).SetupWithManager(k8sManager)
+	Expect(err).ToNot(HaveOccurred())
+
+	go func() {
+		defer GinkgoRecover()
+		err = k8sManager.Start(ctx)
+		Expect(err).ToNot(HaveOccurred(), "failed to run manager")
+	}()
+
 })
 
 var _ = AfterSuite(func() {
+	cancel()
 	By("tearing down the test environment")
 	err := testEnv.Stop()
 	Expect(err).NotTo(HaveOccurred())
