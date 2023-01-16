e0712b225e7b39dbc5f3a8a3f90c54bbd50f5008
Dimitar Ralev <dimitar@rallyo.io>
2023-01-16 18:33:05 +0200

    Expose k8s client (#14)
    Expose the Kubernetes client

--- clients/clients.go
+++ clients/clients.go
@@ -3,6 +3,15 @@ package clients
 
 import (
 	"errors"
+	"flag"
+	"os"
+
+	managerv1 "github.com/kotaicode/resource-booking-operator/api/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
 const (
@@ -28,6 +37,22 @@ type CloudResource interface {
 	Status() (ResourceStatus, error)
 }
 
+var kubeconfig string
+
+func init() {
+	// Check for kube config. Priority is:
+	// 1. --kubeconfig flag.
+	// 2. KUBECONFIG env variable
+	// 3. Default = /.kube/config
+	kubeconfig = flag.Lookup("kubeconfig").Value.String()
+	if kubeconfig == "" {
+		kubeconfig, _ = os.LookupEnv("KUBECONFIG")
+		if kubeconfig == "" {
+			kubeconfig = "/.kube/config"
+		}
+	}
+}
+
 // ResourceFactory generates structs that abide by the CloudResource interface.
 // The returned struct can start, stop, and list instances. Each new integration needso to be added to this factory function.
 func ResourceFactory(resType, tag string) (CloudResource, error) {
@@ -41,4 +66,30 @@ func ResourceFactory(resType, tag string) (CloudResource, error) {
 	}
 
 	return resource, nil
+}
+
+// GetClient returns a ready to use kubernetes client.
+func GetClient() (client.Client, error) {
+	var err error
+	var config *rest.Config
+
+	config, err = rest.InClusterConfig()
+	if err != nil {
+		config, err = clientcmd.BuildConfigFromFlags("", kubeconfig)
+		if err != nil {
+			panic(err.Error())
+		}
+	}
+
+	scheme := runtime.NewScheme()
+	utilruntime.Must(managerv1.AddToScheme(scheme))
+	clientOpts := client.Options{Scheme: scheme}
+
+	c, err := client.New(config, clientOpts)
+	if err != nil {
+		return nil, err
+	}
+
+	return c, nil
+
 }
