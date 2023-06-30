bcc4606e43dceda27e93e8eb53d7d3a481d94422
Dimitar Ralev <dimitar@rallyo.io>
2023-06-30 09:16:52 +0300

    Fix missing rbac definitions

--- controllers/bookingscheduler_controller.go
+++ controllers/bookingscheduler_controller.go
@@ -36,6 +36,10 @@ type BookingSchedulerReconciler struct {
 	Scheme *runtime.Scheme
 }
 
+//+kubebuilder:rbac:groups=manager.kotaico.de,resources=bookingschedulers,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=manager.kotaico.de,resources=bookingschedulers/status,verbs=get;update;patch
+//+kubebuilder:rbac:groups=manager.kotaico.de,resources=bookingschedulers/finalizers,verbs=update
+
 // For more details, check Reconcile and its Result here:
 // - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.13.0/pkg/reconcile
 func (r *BookingSchedulerReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
